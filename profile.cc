e22756bddf591f0c6ca27c4d600a05cc403a3fc7
MohamedElm1678 <mohamedapple@hotmail.com>
2019-05-21 11:27:18 -0700

    Account for potentially not reaching max velocity

--- profile.cc
+++ profile.cc
@@ -2,30 +2,34 @@
 
 const double Profile::GetTime(bool ignore) {
   double delta_x = goal_.position - current_.position;
-  double discrim = (current_.velocity * current_.velocity) + (2 * kMaxAcceleration * delta_x);
-  
-  if (ignore) {  
-    double full_send_time = (current_.velocity + sqrt(discrim)) / kMaxAcceleration;
-  //  double full_send_time = delta_x / (.5 * (goal_.velocity + current_.velocity));
-  //  double full_send_time = (current_.velocity + sqrt(discrim)) / kMaxAcceleration;
+  double discrim = (current_.velocity * current_.velocity) +
+                   (2 * kMaxAcceleration * delta_x);
+  double max_v = std::sqrt((kMaxAcceleration * std::pow(current_.velocity, 2)) +
+                           2 * kMaxAcceleration * goal_.position *
+                               kMaxAcceleration / 2 * kMaxAcceleration);
+
+  if (ignore) {
+    double full_send_time =
+        (current_.velocity + sqrt(discrim)) / kMaxAcceleration;
+    //  double full_send_time = delta_x / (.5 * (goal_.velocity +
+    //  current_.velocity)); double full_send_time = (current_.velocity +
+    //  sqrt(discrim)) / kMaxAcceleration;
     return full_send_time;
- } else {
-   // first part of trapezoid
-   double accel_time = (goal_.velocity - current_.velocity) / kMaxAcceleration;
-   double accel_dist = .5 * kMaxVelocity * accel_time; 
-   
-   double vel_time = delta_x - (2 * accel_dist);
+  } else {
+    if (max_v == kMaxVelocity) {
+      // first part of trapezoid
+      double accel_time =
+          (goal_.velocity - current_.velocity) / kMaxAcceleration;
+      double accel_dist = .5 * kMaxVelocity * accel_time;
 
-   double total_time = (2 * accel_time) + (vel_time / kMaxVelocity); 
+      double vel_time = delta_x - (2 * accel_dist);
 
-  // double accel_time = (current_.velocity + sqrt(discrim)) / constraints_.kMaxAcceleration;
-  // double dist_2 = delta_x - (accel_time * 2 * constraints_.velocity);
-  // double vel_time = dist_2 / (constraints_.kMaxVelocity);
-  // double accel_time = constraints_.kMaxVelocity - current_.position / constraints_.kMaxAcceleration;
-  // double straight_dist = sqrt((2 * delta_x) / constraints_.kMaxAcceleration);
-  // double vel_time = straight_dist / (.5 * (goal_.velocity + current_.velocity));
-  // double deccel_time = constraints_.kMaxVelocity - current_.position / constraints_.kMaxAcceleration;
+      double total_time = (2 * accel_time) + (vel_time / kMaxVelocity);
 
-   return total_time;
- }
+      return total_time;
+    } else if (max_v < kMaxVelocity) {
+      double total_time = std::sqrt((2 * delta_x) / (kMaxAcceleration + 1.0));
+      return total_time;
+    }
+  }
 }
